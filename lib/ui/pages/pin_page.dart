491ecd9958bbf5fecfa94de62e5b9475a97aab24
Suhendar <hendarsyahss@gmail.com>
2025-06-02 08:01:37 +0700

    Update pin_page.dart

--- lib/ui/pages/pin_page.dart
+++ lib/ui/pages/pin_page.dart
@@ -1,7 +1,9 @@
+import 'package:bank_sha/blocs/auth/auth_bloc.dart';
 import 'package:bank_sha/shared/shared_methods.dart';
 import 'package:bank_sha/shared/theme.dart';
 import 'package:bank_sha/ui/widgets/buttons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class PinPage extends StatefulWidget {
   const PinPage({super.key});
@@ -12,6 +14,8 @@ class PinPage extends StatefulWidget {
 
 class _PinPageState extends State<PinPage> {
   final TextEditingController pinController = TextEditingController(text: "");
+  String pin = '';
+  bool isError = false;
 
   addPin(String number) {
     if (pinController.text.length < 6) {
@@ -21,9 +25,12 @@ class _PinPageState extends State<PinPage> {
     }
 
     if (pinController.text.length == 6) {
-      if (pinController.text == "123123") {
+      if (pinController.text == pin) {
         Navigator.pop(context, true);
       } else {
+        setState(() {
+          isError = true;
+        });
         showCustomSnackbar(
           context,
           "Pin yang anda masukan salah, Silahkan coba lagi",
@@ -37,6 +44,7 @@ class _PinPageState extends State<PinPage> {
   deletePin() {
     if (pinController.text.isNotEmpty) {
       setState(() {
+        isError = false;
         pinController.text = pinController.text.substring(
           0,
           pinController.text.length - 1,
@@ -44,6 +52,16 @@ class _PinPageState extends State<PinPage> {
       });
 
       //debugPrint("number ${pinController.text}");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    final authState = context.read<AuthBloc>().state;
+
+    if (authState is AuthSuccess) {
+      pin = authState.user.pin!;
     }
   }
 
@@ -77,6 +95,7 @@ class _PinPageState extends State<PinPage> {
                     fontSize: 36,
                     fontWeight: medium,
                     letterSpacing: 16,
+                    color: isError ? redColor : whiteColor,
                   ),
                   maxLength: 6,
                   decoration: InputDecoration(
